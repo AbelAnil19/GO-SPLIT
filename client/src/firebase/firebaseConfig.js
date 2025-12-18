6f277087e98ec558d7d9c7c52931b1c9a76c6448
Abel Anil Mathew <abelanilmathew2028@mca.ajce.in>
2025-12-18 11:16:08 +0530

    Env
    Made api more safe

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // TODO: Replace with your project's config object
 const firebaseConfig = {
-    apiKey: "AIzaSyD_Dz9oDjS7_okCBEyZJEFqRs1-DZhn5CE",
-    authDomain: "gosplit-5111a.firebaseapp.com",
-    projectId: "gosplit-5111a",
-    storageBucket: "gosplit-5111a.firebasestorage.app",
-    messagingSenderId: "906692361516",
-    appId: "1:906692361516:web:90418d58113fb739294089"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
